eb5fc30f5564c8c147be0a14a21eb4112dcf7452
Azhovan Asadi <azhovan@one.fit>
2019-12-14 11:48:03 +0100

    improve ast structure, introduce more flexibility

--- ast.go
+++ ast.go
@@ -1,24 +1,36 @@
-package BrainFuck
-
-// Token, represent a lexical tokens
-type Token int
+package main
 
 const (
-
-	// Language Keywords
-	LEFT   Token = iota // <
-	RIGHT               // >
-	PLUS                // +
-	MINUS               // -
-	PRINT               // .
-	READ                // ,
-	LBRACK              // [
-	RBRACK              // ]
-
-	WS      // white space
-	ILLEGAL //illegal tokens (like digits, letters)
+	IllegalToken      Tok = iota
+	LeftToken             // <
+	RightToken            // >
+	PlusToken             // +
+	MinusToken            // -
+	PrintToken            // .
+	ReadToken             // ,
+	LeftBracketToken      // [
+	RightBracketToken     // ]
+	WhitespaceToken
 )
 
-// special var to indicate end of stream
+// special var to indicate end of stream.
 var EOF = rune(0)
 
+// Tok represents a lexical token type.
+type Tok int
+
+// Token, represent a lexical tokens.
+type Token struct {
+	// the type of token.
+	Tok Tok
+
+	// The literal value of the token(as parsed).
+	Value string
+
+	// The rune used for string tokens
+	Ending rune
+
+	// Used for numeric tokens.
+	Number float64
+	Unit   string
+}
